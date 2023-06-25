c56b885afa262b207caa710b39c431a8531f7216
ClarkKent2056 <luckov097@gmail.com>
2023-06-25 15:35:16 +0200

    1.0.3.1

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const menu = document.querySelector('.menu')
 const menuLink = document.querySelector('.navheader')
 
-menu.onclick = () => {
+menu.addEventListener('click' = () => {
     menu.classList.toggle('open')
     menuLink.classList.toggle('close')
-}
+})
 menu.addEventListener('touchstart', ()=>{
     menu.classList.toggle('open')
     menuLink.classList.toggle('close')
